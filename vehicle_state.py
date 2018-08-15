0869b939184daa4f1a89265fcc8f8c322d141875
Abdurrahman <abdur.rahman@tokopedia.com>
2018-08-15 13:40:09 +0700

    Changelog:
    - Return altitude global relative frame instead of MSL altitude.

--- vehicle_state.py
+++ vehicle_state.py
@@ -13,7 +13,7 @@ def on_connect(client, userdata, flags, rc):
     # #-- Connect to the vehicle
     print('Connecting...')
     global vehicle
-    # vehicle = connect('tcp:192.168.43.51:5760', wait_ready=False)
+    # vehicle = connect('localhost:14552', wait_ready=False)
     vehicle = connect('/dev/ttyUSB0', wait_ready=False)
     
     @vehicle.on_attribute("mode")
@@ -25,8 +25,8 @@ def on_connect(client, userdata, flags, rc):
         client.publish("/drone-status", "pitch:" + "%.3f" % math.degrees(value.pitch) + "|roll:" + "%.3f" % math.degrees(value.roll) + "|yaw:" + "%.3f" % math.degrees(value.yaw))
         client.publish("/drone-status", "mode:" + vehicle.mode.name + "&armed:" + str(vehicle.armed))
 
-    @vehicle.on_attribute("location.global_frame")
-    def global_frame_change_listener(self, attr_name, value):
+    @vehicle.on_attribute("location.global_relative_frame")
+    def global_relative_frame_change_listener(self, attr_name, value):
         client.publish("/drone-status", "lat:" + "%s" % value.lat + ";lng:" + "%s" % value.lon + ";alt:" + "%s" % value.alt)
     
 client = mqtt.Client()
